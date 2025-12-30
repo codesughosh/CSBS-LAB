8b80fc8ce28698139d2d9486b48bb99a43a43e12
Sughosh Kulkarni <sjce.sughosh@gmail.com>
2025-12-30 20:51:38 +0530

    Update 1.cpp

--- Object Oriented Programming/1.cpp
+++ Object Oriented Programming/1.cpp
@@ -1,49 +1,65 @@
 #include <iostream>
 using namespace std;
 
-class EMPLOYEE {
+class EMPLOYEE
+{
+private:
     int Employee_Number;
     string Employee_Name;
     float Basic, DA, IT, Net_Sal;
 
 public:
-    void read() {
+    void readData()
+    {
         cout << "Enter Employee Number: ";
         cin >> Employee_Number;
+
         cout << "Enter Employee Name: ";
         cin >> Employee_Name;
+
         cout << "Enter Basic Salary: ";
         cin >> Basic;
     }
 
-    void calculate() {
+    void calculateNetSal()
+    {
         DA = 0.52 * Basic;
         float gross = Basic + DA;
-        IT = 0.3 * gross;
+        IT = 0.30 * gross;
         Net_Sal = gross - IT;
     }
 
-    void display() {
+    void printData()
+    {
         cout << "\nEmployee Number: " << Employee_Number;
         cout << "\nEmployee Name: " << Employee_Name;
-        cout << "\nBasic: " << Basic;
+        cout << "\nBasic Salary: " << Basic;
         cout << "\nDA: " << DA;
-        cout << "\nIT: " << IT;
-        cout << "\nNet Salary: " << Net_Sal << "\n";
+        cout << "\nIncome Tax: " << IT;
+        cout << "\nNet Salary: " << Net_Sal << endl;
     }
 };
 
-int main() {
+int main()
+{
     int N;
     cout << "Enter number of employees: ";
     cin >> N;
-    EMPLOYEE e[N];
-    for (int i = 0; i < N; i++) {
-        cout << "\nEnter details for Employee " << i + 1 << ":\n";
-        e[i].read();
-        e[i].calculate();
+
+    EMPLOYEE emp[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << "\nEnter details of Employee " << i + 1 << endl;
+        emp[i].readData();
+        emp[i].calculateNetSal();
     }
-    cout << "\nEmployee Details:\n";
-    for (int i = 0; i < N; i++) e[i].display();
+
+    cout << "\n--- Employee Salary Details ---\n";
+    for (int i = 0; i < N; i++)
+    {
+        emp[i].printData();
+    }
+
     return 0;
 }
