80e868813c0210acb9aa729bd236b2adf175b5b5
Sughosh Kulkarni <sjce.sughosh@gmail.com>
2025-12-30 20:52:29 +0530

    Update 3.cpp

--- Object Oriented Programming/3.cpp
+++ Object Oriented Programming/3.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
 using namespace std;
 
-class COMPLEX {
-    float real, imag;
+class COMPLEX
+{
+private:
+    int real, imag;
+
 public:
-    void read() {
-        cout << "Enter real and imaginary parts: ";
-        cin >> real >> imag;
+    void read()
+    {
+        cout << "Enter real part: ";
+        cin >> real;
+        cout << "Enter imaginary part: ";
+        cin >> imag;
     }
-    void display() {
-        if (imag >= 0)
-            cout << real << " + " << imag << "i" << endl;
-        else
-            cout << real << " - " << -imag << "i" << endl;
+
+    void display()
+    {
+        cout << real << " + " << imag << "i" << endl;
     }
-    COMPLEX ADD(int a, COMPLEX s2) {
+
+    // ADD(int, COMPLEX)
+    COMPLEX ADD(int a, COMPLEX s2)
+    {
         COMPLEX temp;
         temp.real = a + s2.real;
         temp.imag = s2.imag;
         return temp;
     }
-    COMPLEX ADD(COMPLEX s1, COMPLEX s2) {
+
+    // ADD(COMPLEX, COMPLEX)
+    COMPLEX ADD(COMPLEX s1, COMPLEX s2)
+    {
         COMPLEX temp;
         temp.real = s1.real + s2.real;
         temp.imag = s1.imag + s2.imag;
@@ -28,17 +39,45 @@ public:
     }
 };
 
-int main() {
-    COMPLEX c1, c2, c3, c4;
-    cout << "Enter first complex number:\n";
-    c1.read();
-    cout << "Enter second complex number:\n";
-    c2.read();
-    c3 = c3.ADD(c1, c2);
-    c4 = c4.ADD(5, c2);
-    cout << "\nSum of two complex numbers: ";
-    c3.display();
-    cout << "Sum of integer and complex number: ";
-    c4.display();
+int main()
+{
+    COMPLEX s1, s2, result;
+    int a, choice;
+
+    cout << "1. ADD (int, COMPLEX)\n";
+    cout << "2. ADD (COMPLEX, COMPLEX)\n";
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        cout << "Enter integer value: ";
+        cin >> a;
+
+        cout << "Enter complex number:\n";
+        s2.read();
+
+        result = result.ADD(a, s2);
+        cout << "Result: ";
+        result.display();
+        break;
+
+    case 2:
+        cout << "Enter first complex number:\n";
+        s1.read();
+
+        cout << "Enter second complex number:\n";
+        s2.read();
+
+        result = result.ADD(s1, s2);
+        cout << "Result: ";
+        result.display();
+        break;
+
+    default:
+        cout << "Invalid choice";
+    }
+
     return 0;
 }
